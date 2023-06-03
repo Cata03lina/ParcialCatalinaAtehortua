bcbb0618bb19a7a09b66b6b82cba1a6e4ed3a2ee
Cata03lina <catalinaatehortua31161@correo.itm.edu.co>
2023-06-02 20:14:27 -0500

    New WebApi project with new Ticket entity

---
+++ ParcialCatalinaAtehortua/ParcialCatalinaAtehortua/DAL/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcialCatalinaAtehortua.Entities;
+
+namespace ParcialCatalinaAtehortua.DAL
+{
+    public class DataBaseContext : DbContext
+
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket>Tickets { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t => t.Id).IsUnique();
+        }
+
+    }
+}
---
+++ ParcialCatalinaAtehortua/ParcialCatalinaAtehortua/DAL/Entities/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialCatalinaAtehortua.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Display(Name ="Fecha de uso")]
+        public DateTime? UseDate { get; set; }
+        [Display(Name ="Ocupada")]
+        public bool IsUsed { get; set; }
+        [Display(Name ="Portería")]
+        [Required]
+        [MaxLength(20)]
+        public string? EntranceGate { get; set; }
+
+    }
+}
